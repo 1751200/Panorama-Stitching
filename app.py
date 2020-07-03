deabfc55924a1abdf05ce4b1e411066caa7837a2
VodkaSoul <vodkasoul@icloud.com>
2020-07-03 18:20:39 +0800

    Add Stitcher

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ For reference I've implemented an example of file upload here. It's available in
 """
 from typing import Dict
 import streamlit as st
-from PIL import Image
+import numpy as np
+import cv2
 
-STYLE = """
-<style>
-img {
-    max-width: 100%;
-}
-</style>
-"""
+import Stitcher
 
 FILE_TYPES = ["png", "jpg", "jpeg"]
 
@@ -26,6 +21,13 @@ def get_static_store() -> Dict:
     return {}
 
 
+def cv2_read_img(io_bytes):
+    io_bytes.seek(0)
+    file_bytes = np.asarray(bytearray(io_bytes.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    return img
+
+
 def main():
     """Run this function to run the app"""
     static_store = get_static_store()
@@ -33,28 +35,33 @@ def main():
     st.sidebar.title("Multiple Image Stitching")
 
     st.info(__doc__)
-    st.markdown(STYLE, unsafe_allow_html=True)
     result = st.file_uploader("Upload your images", type=FILE_TYPES)
     if result:
         # Process you file here
         value = result.getvalue()
 
         # And add it to the static_store if not already in
-        if not value in static_store.values():
+        if value not in static_store.values():
             static_store[result] = value
     else:
         static_store.clear()  # Hack to clear list if the user clears the cache and reloads the page
-        st.info("Upload one or more images of type ." + ", ".join(FILE_TYPES))
+        st.info("Upload one or more images of type ." + ", .".join(FILE_TYPES))
 
-    if st.button("Clear file list"):
+    if st.button("Clear image list"):
         static_store.clear()
-    if st.checkbox("Show file list?", True):
+    if st.checkbox("Show image list?", True):
         st.write(list(static_store.keys()))
-    if st.checkbox("Show content of files?"):
+    if st.checkbox("Show content of images?"):
         images = []
-        for value in static_store.values():
-            images.append(value)
-        if images != []:
-            st.image(images, width=int(650/len(images)), caption=[str(i) for i in range(len(images))])
+        for key in static_store.keys():
+            images.append(cv2_read_img(key))
+        if images:
+            st.image(images, width=int(630/len(images)), channels='BGR',
+                     caption=[str(i) for i in range(len(images))])
+    if st.button("Stitch Images"):
+        s = Stitcher.Stitch([cv2_read_img(key) for key in static_store.keys()])
+        s.leftshift()
+        s.rightshift()
+        st.image(s.leftImage, use_column_width=True, channels='BGR', caption='Stitch Result')
 
 main()
